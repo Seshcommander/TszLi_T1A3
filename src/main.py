575bb3c28c513e93b4b0be266f59f714fa79f167
Peter Li <peteyhub@gmail.com>
2022-07-09 17:06:20 +1000

    updated options menu and testing price check function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from os import system
 import pandas as pd
+
 # imported pandas library as pd
 df = pd.read_csv(
     "/Users/petey/CoderAcademy/TszLi_T1A3/docs/btcusdt.csv", usecols=[
@@ -6,14 +8,16 @@ df = pd.read_csv(
             "unix": int, "date": str, "symbol": str, "open": float, "high": float, "low": float, "close": float, "Volume BTC": float, "Volume USDT": float
     })
 # dataframe variable assigned to pd.read_csv function with input parameters specifying file location, columns and data types
-# print(df["date"].dtypes)
 
 
 def welcome_message_BTC():
     print("Welcome to the BTC historical price/volume checker!\n To begin, please select from the following options:")
+# Welcome message to user
 
 
-welcome_message_BTC()
+# welcome_message_BTC()
+
+# function to display options 1-4, user input is saved as opt var
 
 
 def print_options():
@@ -21,13 +25,24 @@ def print_options():
     print("2) Price comparison between two dates")
     print("3) Check volume of BTC")
     print("4) Exit")
+    opt = input("Select your option (1-4): ")
+    return opt
+
+# price check input function to receive user input and check against
 
 
-opt = input("Select your option (1-4): ")
+def user_input_date():
+    user_date = input("Please enter a date in the format (YYYY-MM-DD): ")
+    return user_date
+
+
+date_to_check = user_input_date
 
 
 def price_check_input():
-    print(f"")
+    # df.loc[df["date"] == date_to_check]
+    # return
+    pass
 
 
 def price_comparison():
@@ -36,3 +51,28 @@ def price_comparison():
 
 def volume_check_input():
     pass
+
+
+option = ""
+
+while option != "4":
+    system("clear")
+    option = print_options()
+    system("clear")
+    if option == "1":
+        user_input_date()
+        price_check_input()
+    elif option == "2":
+        price_comparison()
+    elif option == "3":
+        volume_check_input()
+    elif option == "4":
+        continue
+    else:
+        print("Invalid option")
+
+    input("Press Enter to continue...")
+    system("clear")
+# print(df[["date", "close"]])
+print()
+print("Goodbye have a great time!")
