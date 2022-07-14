b0cbfdc1cd3d45c1ff13e94886670e4bdbc66a5c
Peter Li <peteyhub@gmail.com>
2022-07-14 16:37:20 +1000

    adjusted price check function and built price comparison function

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def welcome_message_BTC():
 
 def print_options():
     print("1) Check historical price of BTC")
-    print("2) Price comparison between two dates")
+    print("2) Price comparison between today and entered date")
     print("3) Check volume of BTC")
     print("4) Exit")
     opt = input("Select your option (1-4): ")
@@ -38,13 +38,22 @@ def user_input_date():
 
 
 def price_check_input():
-    pass
+    user_date = user_input_date()
+    # exact dates
+    print(float(df.loc[(df['date'] == user_date)]['close']))
 # At a loss for how price check input can take user_enetered_date var and check it against the pd data frame of the csv.
 # As per above, need to have a way of matching user_date YYYY-MM-DD to the corresponding row that has the exact date
 
 
 def price_comparison():
-    pass
+    user_comparison_date = user_input_date()
+    user_close_price = float(
+        df.loc[(df['date'] == user_comparison_date)]['close'])
+    current_close_price = float(df.loc[(df['date'] == '2022-07-07')]['close'])
+    print(f"The close price on your chosen date is {user_close_price}")
+    print(f"The current close price of BTC is {current_close_price}")
+
+
 # this functions purpose is to allow the user to enter a date they want the price of btc at and it will calculate the % gain/loss against the most recent updated price (line 2 of the CSV)
 
 
@@ -62,14 +71,12 @@ while option != "4":
     option = print_options()
     system("clear")
     if option == "1":
-        user_date = user_input_date()
-        # exact dates
-        print(df.loc[(df['date'] == user_date)])
+        price_check_input()
     elif option == "2":
         price_comparison()
         # between two dates
-        print(df.loc[(df['date'] >= '2017-10-23 00:00:00')
-                     & (df['date'] < '2017-10-27 00:00:00')])
+        # print(df.loc[(df['date'] >= '2017-10-23 00:00:00')
+        #              & (df['date'] < '2017-10-27 00:00:00')])
     elif option == "3":
         volume_check_input()
     elif option == "4":
